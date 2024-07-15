b0541e844464fd5f9277c5a483a61853e3531200
Prisca <prisca.castanier@gmail.com>
2024-07-16 00:37:56 +0200

    feat(route): close exchange to avoid issue with exchange not closing

--- src/main/kotlin/response/StatusResponse.kt
+++ src/main/kotlin/response/StatusResponse.kt
@@ -6,40 +6,48 @@ open class StatusResponse() {
     fun ok(exchange: HttpExchange, response: String = "ok") {
         exchange.sendResponseHeaders(200, response.length.toLong())
         exchange.responseBody.use { it.write(response.toByteArray()) }
+        exchange.close()
     }
 
     fun created(exchange: HttpExchange, response: String = "created") {
         exchange.sendResponseHeaders(201, response.length.toLong())
         exchange.responseBody.use { it.write(response.toByteArray()) }
+        exchange.close()
     }
 
     fun badRequest(exchange: HttpExchange, response: String = "bad request") {
         exchange.sendResponseHeaders(400, response.length.toLong())
         exchange.responseBody.use { it.write(response.toByteArray()) }
+        exchange.close()
     }
 
     fun unauthorized(exchange: HttpExchange, response: String = "unauthorized") {
         exchange.sendResponseHeaders(401, response.length.toLong())
         exchange.responseBody.use { it.write(response.toByteArray()) }
+        exchange.close()
     }
 
     fun forbidden(exchange: HttpExchange, response: String = "forbidden") {
         exchange.sendResponseHeaders(403, response.length.toLong())
         exchange.responseBody.use { it.write(response.toByteArray()) }
+        exchange.close()
     }
 
     fun notFound(exchange: HttpExchange, response: String = "not found") {
         exchange.sendResponseHeaders(404, response.length.toLong())
         exchange.responseBody.use { it.write(response.toByteArray()) }
+        exchange.close()
     }
 
     fun methodNotAllowed(exchange: HttpExchange, response: String = "method not allowed") {
         exchange.sendResponseHeaders(405, response.length.toLong())
         exchange.responseBody.use { it.write(response.toByteArray()) }
+        exchange.close()
     }
 
     fun internalServerError(exchange: HttpExchange, response: String = "internal server error") {
         exchange.sendResponseHeaders(500, response.length.toLong())
         exchange.responseBody.use { it.write(response.toByteArray()) }
+        exchange.close()
     }
 }
