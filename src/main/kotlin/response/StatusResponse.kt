0df3a1c433951390b15fe03df08051c0722b3466
Prisca <prisca.castanier@gmail.com>
2024-07-15 17:46:50 +0200

    feat(status): add class to handle http response

---
+++ src/main/kotlin/response/StatusResponse.kt
@@ -0,0 +1,45 @@
+package org.example.response
+
+import com.sun.net.httpserver.HttpExchange
+
+open class StatusResponse() {
+    fun ok(exchange: HttpExchange, response: String = "ok") {
+        exchange.sendResponseHeaders(200, response.length.toLong())
+        exchange.responseBody.use { it.write(response.toByteArray()) }
+    }
+
+    fun created(exchange: HttpExchange, response: String = "created") {
+        exchange.sendResponseHeaders(201, response.length.toLong())
+        exchange.responseBody.use { it.write(response.toByteArray()) }
+    }
+
+    fun badRequest(exchange: HttpExchange, response: String = "bad request") {
+        exchange.sendResponseHeaders(400, response.length.toLong())
+        exchange.responseBody.use { it.write(response.toByteArray()) }
+    }
+
+    fun unauthorized(exchange: HttpExchange, response: String = "unauthorized") {
+        exchange.sendResponseHeaders(401, response.length.toLong())
+        exchange.responseBody.use { it.write(response.toByteArray()) }
+    }
+
+    fun forbidden(exchange: HttpExchange, response: String = "forbidden") {
+        exchange.sendResponseHeaders(403, response.length.toLong())
+        exchange.responseBody.use { it.write(response.toByteArray()) }
+    }
+
+    fun notFound(exchange: HttpExchange, response: String = "not found") {
+        exchange.sendResponseHeaders(404, response.length.toLong())
+        exchange.responseBody.use { it.write(response.toByteArray()) }
+    }
+
+    fun methodNotAllowed(exchange: HttpExchange, response: String = "method not allowed") {
+        exchange.sendResponseHeaders(405, response.length.toLong())
+        exchange.responseBody.use { it.write(response.toByteArray()) }
+    }
+
+    fun internalServerError(exchange: HttpExchange, response: String = "internal server error") {
+        exchange.sendResponseHeaders(500, response.length.toLong())
+        exchange.responseBody.use { it.write(response.toByteArray()) }
+    }
+}
